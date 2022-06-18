6c58521babcae0ee7bedf2f4210229fc5ba14266
hariyono1995 <onnelikrio@gmail.com>
2022-06-18 21:52:59 +0700

    ..

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -5,6 +5,7 @@ import { Box, Stack, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "utils/fetchData";
 
 import { ExerciseCard } from "./ExerciseCard";
+import Loader from "./Loader";
 
 export const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -43,6 +44,8 @@ export const Exercises = ({ exercises, setExercises, bodyPart }) => {
     };
     fetchExercisesData();
   }, [bodyPart]);
+
+  if (!currentExercises.length) return <Loader />;
 
   return (
     <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
