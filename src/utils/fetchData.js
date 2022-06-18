872450de2e6e8d909ef88b39fd4dc97f262f1c22
hariyono1995 <onnelikrio@gmail.com>
2022-06-18 21:51:23 +0700

    ...

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,14 @@ export const exerciseOptions = {
   },
 };
 
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5a62d3a438mshb624e364b3aef9ep1ca706jsn8423fb77a162",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
+
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
   const data = await response.json();
