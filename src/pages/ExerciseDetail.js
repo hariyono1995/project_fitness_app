0f7b9e1262c2fa94a33f97db4703a14363559a08
hariyono1995 <onnelikrio@gmail.com>
2022-06-20 09:42:54 +0700

    ....

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -54,11 +54,11 @@ export const ExerciseDetail = () => {
 
   return (
     <Box sx={{ mt: { lg: "96px", xs: "60px" } }}>
-      {/* <Detail exerciseDetail={exerciseDetail} />
+      <Detail exerciseDetail={exerciseDetail} />
       <ExerciseVideos
         exerciseVideos={exerciseVideos}
         name={exerciseDetail.name}
-      /> */}
+      />
       <SimilarExercises
         targetMuscleExercises={targetMuscleExercises}
         equipmentExercises={equipmentExercises}
